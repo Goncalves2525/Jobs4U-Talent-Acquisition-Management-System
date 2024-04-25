95fdd7ca5a87650852b1d0fed37e0b16765fd754
1212047-rafaelcarolo <1212047@isep.ipp.pt>
2024-04-26 00:12:00 +0100

    Formatted classes and pom. No function changes.

--- jobs4u.infrastructure.application/src/main/java/settings/AppSettings.java
+++ jobs4u.infrastructure.application/src/main/java/settings/AppSettings.java
@@ -1,26 +1,4 @@
-package settings;/*
- * Copyright (c) 2013-2024 the original author or authors.
- *
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+package settings;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -34,7 +12,7 @@ import java.util.Properties;
 
 /**
  * The application settings.
- *
+ * Adapted from:
  * @author Paulo Gandra Sousa
  */
 public class AppSettings {
@@ -68,8 +46,7 @@ public class AppSettings {
 	}
 
 	private void setDefaultProperties() {
-		applicationProperties.setProperty(REPOSITORY_FACTORY_KEY,
-				"jpa.JpaRepositoryFactory");
+		applicationProperties.setProperty(REPOSITORY_FACTORY_KEY, "jpa.JpaRepositoryFactory");
 		applicationProperties.setProperty(UI_MENU_LAYOUT_KEY, "horizontal");
 		applicationProperties.setProperty(PERSISTENCE_UNIT_KEY, "default");
 	}
