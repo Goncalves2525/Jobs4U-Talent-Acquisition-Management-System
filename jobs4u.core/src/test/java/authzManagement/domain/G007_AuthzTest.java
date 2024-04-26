ffb869bbd8885425d4dea6ebe84a7299bdba3e6d
1212047-rafaelcarolo <1212047@isep.ipp.pt>
2024-04-26 01:27:23 +0100

    Added US G007 JUnit test. #8

---
+++ jobs4u.core/src/test/java/authzManagement/domain/G007_AuthzTest.java
@@ -0,0 +1,52 @@
+package authzManagement.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class G007_AuthzTest {
+
+    // TODO: review it
+//    @Test
+//    void verifyThatOnlyAnEmailIsValidOnUserField() {
+//        String invalidEmail1 = "username";
+//        String invalidEmail2 = "username@domain";
+//        String invalidEmail3 = "username@domain";
+//        String validEmail1 = "username@domain.com";
+//
+//        Email email1 = (Email) Email.tryValueOf(invalidEmail1).right();
+//        Email email2 = (Email) Email.tryValueOf(invalidEmail2).right();
+//        Email email3 = (Email) Email.tryValueOf(invalidEmail3).right();
+//        Email email4 = (Email) Email.tryValueOf(validEmail1).right();
+//
+//        System.out.println(email1);
+//        System.out.println(email2);
+//        System.out.println(email3);
+//        System.out.println(email4);
+//
+//        assertTrue(email1.isEmpty());
+//        assertTrue(email2.isEmpty());
+//        assertTrue(email3.isEmpty());
+//        assertFalse(email4.isEmpty());
+//    }
+
+    @Test
+    public void isPasswordValidationGuaranteingRules(){
+        Password pwd = new Password();
+
+        boolean invalidBadLength = pwd.createPassword("123");
+        boolean invalidMissingUpper = pwd.createPassword("abc123!!!");
+        boolean invalidMissingLower = pwd.createPassword("ABC123!!!");
+        boolean invalidMissingNumber = pwd.createPassword("ABCabc!!!");
+        boolean invalidMissingSymbol = pwd.createPassword("ABCabc123");
+        boolean valid = pwd.createPassword("ABCabc123!!!");
+
+        assertFalse(invalidBadLength);
+        assertFalse(invalidMissingUpper);
+        assertFalse(invalidMissingLower);
+        assertFalse(invalidMissingNumber);
+        assertFalse(invalidMissingSymbol);
+        assertTrue(valid);
+    }
+
+}
