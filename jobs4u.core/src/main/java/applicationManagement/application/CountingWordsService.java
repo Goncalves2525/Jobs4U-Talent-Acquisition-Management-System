1353cb06a23a0f42941f9cb9f1da500736a8c31b
1212047-rafaelcarolo <1212047@isep.ipp.pt>
2024-05-30 22:10:56 +0100

    #44 Created CountingWordsService and OrderingService classes

---
+++ jobs4u.core/src/main/java/applicationManagement/application/CountingWordsService.java
@@ -0,0 +1,12 @@
+package applicationManagement.application;
+
+public class CountingWordsService {
+
+    // JORGE
+    // recebe file paths e faz a contagem
+    // thread principal
+
+    // RAFAEL
+    // ficheiro / caminho ficheiro
+    // no ficheiro, distribuir por threads um x nr de linhas
+}
---
+++ jobs4u.core/src/main/java/applicationManagement/application/OrderingService.java
@@ -0,0 +1,11 @@
+package applicationManagement.application;
+
+import java.util.HashMap;
+
+public class OrderingService {
+
+//    HashMap<Palavra, Conteudo>
+//
+//    int quantidade
+//           List<String> ficheiros
+}
