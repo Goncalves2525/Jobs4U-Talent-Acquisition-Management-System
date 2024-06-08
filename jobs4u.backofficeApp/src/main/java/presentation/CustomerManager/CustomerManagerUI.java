5c12135608b7afd262082f23d9da583ff7cca015
ricardo <1221720@isep.ipp.pt>
2024-06-08 12:04:33 +0100

    Changed Customer Manager UI

--- jobs4u.backofficeApp/src/main/java/presentation/CustomerManager/CustomerManagerUI.java
+++ jobs4u.backofficeApp/src/main/java/presentation/CustomerManager/CustomerManagerUI.java
@@ -19,19 +19,19 @@ public class CustomerManagerUI {
 
         options.add("Register Customer");                               // 1
         options.add("Register Job Opening");                            // 2
-        options.add("List Job Openings");                               // 3
-        options.add("Select Interview Model");                          // 4
+        options.add("Edit Job Opening");                                // 3
+        options.add("List Job Openings");                               // 4
         options.add("List Candidate Personal Data");                    // 5
-        options.add("Test Plugin");                                     // 6
-        options.add("List Applications For Job Opening");               // 7
-        options.add("Generate Interview File");                         // 8
-        options.add("Select Job Requirements Specifications");          // 9
-        options.add("Edit Job Opening");                                // 10
-        options.add("Generate Job Requirement Specification File");     // 11
-        options.add("Check Application Data");                          // 12
-        options.add("Notification Menu");                               // 13
-        options.add("Verify Job Requirements Specification");           // 14
-        options.add("Register Interview Date");                         // 15
+        options.add("List Applications For Job Opening");               // 6
+        options.add("Generate Job Requirement Specification File");     // 7
+        options.add("Select Job Requirements Specifications File");     // 8
+        options.add("Verify Job Requirements Specification File");      // 9
+        options.add("Register Interview Date");                         // 10
+        options.add("Generate Interview File");                         // 11
+        options.add("Select Interview Model");                          // 12
+        options.add("Evaluate Interview Model");                        // 13
+        options.add("Check Application Data");                          // 14
+        options.add("Notification Menu");                               // 15
 
         String message = "What do you want to do?";
         String exit = "Exit";
@@ -54,56 +54,56 @@ public class CustomerManagerUI {
                     registerJobOpeningUI.doShow(authzUI);
                     break;
                 case 3:
-                    ListJobOpeningsUI listJobOpeningsUI = new ListJobOpeningsUI();
-                    listJobOpeningsUI.doShow(authzUI);
+                    EditJobOpeningUI editJobOpeningUI = new EditJobOpeningUI();
+                    editJobOpeningUI.doShow(authzUI);
                     break;
                 case 4:
-                    SelectInterviewModelUI selectInterviewModelUI = new SelectInterviewModelUI();
-                    selectInterviewModelUI.doShow(authzUI);
+                    ListJobOpeningsUI listJobOpeningsUI = new ListJobOpeningsUI();
+                    listJobOpeningsUI.doShow(authzUI);
                     break;
                 case 5:
                     ListCandidatePersonalDataUI listCandidatePersonalDataUI = new ListCandidatePersonalDataUI();
                     listCandidatePersonalDataUI.doShow(authzUI);
                     break;
                 case 6:
-                    TestPluginUI testPluginUI = new TestPluginUI();
-                    testPluginUI.doShow(authzUI);
-                    break;
-                case 7:
                     ListApplicationsUI listApplicationsUI = new ListApplicationsUI();
                     listApplicationsUI.doShow(authzUI);
                     break;
-                case 8:
-                    GenerateInterviewQuestionsFileUI generateAnswerCollectionFileUI = new GenerateInterviewQuestionsFileUI();
-                    generateAnswerCollectionFileUI.doShow(authzUI);
-                    break;
-                case 9:
-                    SelectJobRequirementSpecificationUI selectJobRequirementSpecificationUI = new SelectJobRequirementSpecificationUI();
-                    selectJobRequirementSpecificationUI.doShow(authzUI);
-                    break;
-                case 10:
-                    EditJobOpeningUI editJobOpeningUI = new EditJobOpeningUI();
-                    editJobOpeningUI.doShow(authzUI);
-                    break;
-                case 11:
+                case 7:
                     GenerateJobRequirementSpecificationQuestionsFileUI generateJobRequirementSpecificationFileUI = new GenerateJobRequirementSpecificationQuestionsFileUI();
                     generateJobRequirementSpecificationFileUI.doShow(authzUI);
                     break;
-                case 12:
-                    CheckApplicationDataUI checkApplicationDataUI = new CheckApplicationDataUI();
-                    checkApplicationDataUI.doShow(authzUI);
+                case 8:
+                    SelectJobRequirementSpecificationUI selectJobRequirementSpecificationUI = new SelectJobRequirementSpecificationUI();
+                    selectJobRequirementSpecificationUI.doShow(authzUI);
                     break;
-                case 13:
-                    NotificationUI notificationUI = new NotificationUI();
-                    notificationUI.doShow(authzUI);
-                    break;
-                case 14:
+                case 9:
                     RequirementsVerificationUI requirementsVerificationUI = new RequirementsVerificationUI();
                     requirementsVerificationUI.doShow(authzUI);
                     break;
-                case 15:
+                case 10:
                     RegisterInterviewDateUI registerInterviewDateUI = new RegisterInterviewDateUI();
                     registerInterviewDateUI.doShow(authzUI);
+                    break;
+                case 11:
+                    GenerateInterviewQuestionsFileUI generateAnswerCollectionFileUI = new GenerateInterviewQuestionsFileUI();
+                    generateAnswerCollectionFileUI.doShow(authzUI);
+                    break;
+                case 12:
+                    SelectInterviewModelUI selectInterviewModelUI = new SelectInterviewModelUI();
+                    selectInterviewModelUI.doShow(authzUI);
+                    break;
+                case 13:
+                    EvaluateInterviewUI evaluateInterviewUI = new EvaluateInterviewUI();
+                    evaluateInterviewUI.doShow(authzUI);
+                    break;
+                case 14:
+                    CheckApplicationDataUI checkApplicationDataUI = new CheckApplicationDataUI();
+                    checkApplicationDataUI.doShow(authzUI);
+                    break;
+                case 15:
+                    NotificationUI notificationUI = new NotificationUI();
+                    notificationUI.doShow(authzUI);
                     break;
                 default:
                     ConsoleUtils.showMessageColor("Invalid option! Try again.", AnsiColor.RED);
---
+++ jobs4u.backofficeApp/src/main/java/presentation/CustomerManager/EvaluateInterviewUI.java
@@ -0,0 +1,9 @@
+package presentation.CustomerManager;
+
+import infrastructure.authz.AuthzUI;
+
+public class EvaluateInterviewUI {
+    public void doShow(AuthzUI authzUI) {
+        System.out.println("Evaluate Interview");
+    }
+}
